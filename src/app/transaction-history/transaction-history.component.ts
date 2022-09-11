1c08729fda0c58980e674e1da4b93fcdd2974db1
Saytzeff-D <davidatanda2020@gmail.com>
2022-09-11 21:52:31 +0100

    trying to compare date here

--- src/app/transaction-history/transaction-history.component.ts
+++ src/app/transaction-history/transaction-history.component.ts
@@ -10,9 +10,11 @@ export class TransactionHistoryComponent implements OnInit {
 
   public history:any = []
   public isLoading = true
+  public today = new Date().toLocaleDateString()
   constructor(public server: LaravelServerService) { }
 
   ngOnInit(): void {
+    console.log(new Date('2022-09-13').toLocaleDateString(), new Date().toLocaleDateString())
     this.server.transHistory().subscribe(data=>{
       this.history = data
       this.isLoading = false
