44b2faa778b032bc662f66799be585f436b8bf9f
Saytzeff-D <davidatanda2020@gmail.com>
2022-09-12 20:06:01 +0100

    responsiveness improved

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm,Validators } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
 import { LaravelServerService } from '../services/laravel-server.service';
@@ -17,39 +17,39 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerInfo = {}
+  registerInfo = { };
 
-  public noSpinnerShow = true
-  public dontShowWord = false
+  public noSpinnerShow = true;
+  public dontShowWord = false;
   constructor(public server: LaravelServerService, public router: Router) { }
-     email = new FormControl('', [Validators.required, Validators.email])
-     password = new FormControl('', [Validators.min(5), Validators.required])
-     confPassword = new FormControl('', [Validators.min(5), Validators.required])
-     firstName = new FormControl('', [Validators.required])
-     lastName = new FormControl('', [Validators.required])
-     mobileNumber = new FormControl('', [Validators.required, Validators.pattern("^((\\+234-?)|0)?[0-9]{10}$")])
+     email = new FormControl('', [Validators.required, Validators.email]);
+     password = new FormControl('', [Validators.min(5), Validators.required]);
+     confPassword = new FormControl('', [Validators.min(5), Validators.required]);
+     firstName = new FormControl('', [Validators.required]);
+     lastName = new FormControl('', [Validators.required]);
+     mobileNumber = new FormControl('', [Validators.required, Validators.pattern('^((\\+234-?)|0)?[0-9]{10}$')]);
   matcher = new MyErrorStateMatcher();
 
   ngOnInit(): void {
   }
 
   register(){
-    let initalRoute = sessionStorage.getItem('route')
-    let msg = (initalRoute == 'Book-Room')?'Login to continue with your bookings':'Login Here! We need to verify that it is you'
+    const initalRoute = sessionStorage.getItem('route');
+    const msg = (initalRoute === 'Book-Room') ? 'Login to continue with your bookings' : 'Login Here! We need to verify that it is you';
     if(this.email.value && this.password.value && this.confPassword.value && this.firstName.value && this.lastName.value && this.mobileNumber.value !== ''){
-      this.noSpinnerShow = false
-      this.dontShowWord = true
+      this.noSpinnerShow = false;
+      this.dontShowWord = true;
     this.registerInfo = {email: this.email.value, password: this.password.value, firstName: this.firstName.value, lastName: this.lastName.value, mobileNumber: this.mobileNumber.value}
     console.log(this.registerInfo)
     this.server.registerUser(this.registerInfo).subscribe(res=>{
-      if (res == 'User Created') {
-        sessionStorage.setItem('successMsg', msg)
-        this.router.navigate(['login'])
+      if (res === 'User Created') {
+        sessionStorage.setItem('successMsg', msg);
+        this.router.navigate(['login']);
       } else {
-        this.noSpinnerShow = true
-        this.dontShowWord = false
-        console.log(res)
-        alert('Unable to process your request')
+        this.noSpinnerShow = true;
+        this.dontShowWord = false;
+        console.log(res);
+        alert('Unable to process your request');
       }
     })
     }
