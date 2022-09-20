fbc826e259c8b49053cd2cc42620c2e62dadcefe
Saytzeff-D <davidatanda2020@gmail.com>
2022-09-20 13:34:41 +0100

    now validating checkIn and checkOut

--- src/app/book-room/book-room.component.ts
+++ src/app/book-room/book-room.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
@@ -14,78 +14,99 @@ import { LaravelServerService } from '../services/laravel-server.service';
 export class BookRoomComponent implements OnInit {
 
   myControl = new FormControl();
-  public roomCategory = ''
-  public savedRoomData:any = JSON.parse(sessionStorage.getItem('roomInfo'))
-  // public savedRoomCategory = (this.savedRoomData.roomCategory !== '')? this.savedRoomData.roomCategory:sessionStorage.getItem('roomCategory')
-  public noSpinnerShow = true
-  public dontShowWord = false
-  public dateDiff:any = ''
-  public categories = ['Family Room', 'Single Room', 'Deluxe Room', 'Executive']
+  public today = new Date().toISOString().split('T')[0];
+  public roomCategory = '';
+  public savedRoomData: any = JSON.parse(sessionStorage.getItem('roomInfo'));
+  public noSpinnerShow = true;
+  public dontShowWord = false;
+  public dateDiff: any = '';
+  public categories = ['Family Room', 'Single Room', 'Deluxe Room', 'Executive'];
 
   public bookRoom = new FormGroup({
-    checkIn: new FormControl(''),
-    checkOut: new FormControl(''),
-    numOfRooms: new FormControl(''),
-    numOfGuests: new FormControl(''),
+    checkIn: new FormControl('', Validators.required),
+    checkOut: new FormControl('', Validators.required),
+    numOfRooms: new FormControl('', Validators.required),
+    numOfGuests: new FormControl('', Validators.required),
+    numOfNights: new FormControl('', Validators.required),
     roomCategory: new FormControl(sessionStorage.getItem('roomCategory'))
-  })
-  matcher = new MyErrorStateMatcher()
+  });
+  matcher = new MyErrorStateMatcher();
   constructor(public server: LaravelServerService, public router: Router) {
-    // this.savedRoomData = sessionStorage.getItem('roomInfo')
   }
-  public error = ''
+  public error = '';
 
   ngOnInit(): void {
-    sessionStorage.removeItem('route')
-    sessionStorage.removeItem('roomInfo')
+    sessionStorage.removeItem('route');
+    sessionStorage.removeItem('roomInfo');
   }
 
-  bookNow(){
-    this.error = ''
-    this.bookRoom.value.numOfNights = this.dateDiff
-    sessionStorage.removeItem('bookRoomInfo')
-    sessionStorage.removeItem('roomCategory')
-    if (this.bookRoom.value.checkIn && this.bookRoom.value.checkOut && this.bookRoom.value.numOfNights && this.bookRoom.value.numOfRooms && this.bookRoom.value.numOfGuests && this.bookRoom.value.roomCategory !== '' && this.bookRoom.value.roomCategory !== null) {
-        this.noSpinnerShow = false
-        this.dontShowWord = true
-        const roomInfo = {roomCategory: this.bookRoom.value.roomCategory, numOfRooms: this.bookRoom.value.numOfRooms}
-        this.server.checkAvailableRoom(roomInfo).subscribe(res=>{
-          console.log(res)
-          if (res == 'Checked') {
-            sessionStorage.setItem('roomInfo', JSON.stringify(this.bookRoom.value))
-            this.router.navigate(['payNow'])
-          } else {
-            this.noSpinnerShow = true
-            this.dontShowWord = false
-            this.error = 'This room is unavailable at the moment'
-          }
-        }, error=>{
-          console.log(error)
-          console.log(error.error.message)
-          if (error.error.message == 'Token has expired' || error.error.message == 'Wrong number of segments') {
-            sessionStorage.setItem('route', 'Book-Room')
-            sessionStorage.setItem('infoMsg', 'Please Login to continue')
-            sessionStorage.setItem('bookRoomInfo', JSON.stringify(this.bookRoom.value))
-            this.router.navigate(['login'])
-          }
-          else{
-            this.noSpinnerShow = true
-            this.dontShowWord = false
-            this.error = 'Unable to Process your request at this time'
-          }
-        })
+  bookNow(): any{
+    this.error = '';
+    // this.bookRoom.value.numOfNights = this.dateDiff
+    sessionStorage.removeItem('bookRoomInfo');
+    sessionStorage.removeItem('roomCategory');
+    if (
+      this.bookRoom.value.checkIn
+      &&
+      this.bookRoom.value.checkOut
+      &&
+      this.bookRoom.value.numOfRooms
+      &&
+      this.bookRoom.value.numOfGuests
+      &&
+      this.bookRoom.value.roomCategory !== ''
+      &&
+      this.bookRoom.value.roomCategory !== null
+    ) {
+      if (this.bookRoom.value.numOfNights === '') {
+        this.error = 'Check out date invalid';
+      }else {
+          this.noSpinnerShow = false;
+          this.dontShowWord = true;
+          const roomInfo = {roomCategory: this.bookRoom.value.roomCategory, numOfRooms: this.bookRoom.value.numOfRooms};
+          this.server.checkAvailableRoom(roomInfo).subscribe(res => {
+            console.log(res)
+            if (res === 'Checked') {
+              sessionStorage.setItem('roomInfo', JSON.stringify(this.bookRoom.value));
+              this.router.navigate(['payNow']);
+            } else {
+              this.noSpinnerShow = true;
+              this.dontShowWord = false;
+              this.error = 'This room is unavailable at the moment';
+            }
+          }, error => {
+            console.log(error)
+            console.log(error.error.message)
+            if (error.error.message == 'Token has expired' || error.error.message == 'Wrong number of segments') {
+              sessionStorage.setItem('route', 'Book-Room')
+              sessionStorage.setItem('infoMsg', 'Please Login to continue')
+              sessionStorage.setItem('bookRoomInfo', JSON.stringify(this.bookRoom.value))
+              this.router.navigate(['login'])
+            }
+            else{
+              this.noSpinnerShow = true;
+              this.dontShowWord = false;
+              this.error = 'Unable to Process your request at this time';
+            }
+          });
+      }
     } else {
-      this.error = 'All fields are Required'
+      this.error = 'All fields are Required';
     }
   }
-  checkDateDiff(){
-    if(this.bookRoom.value.checkIn && this.bookRoom.value.checkOut !== ''){
-      let checkIn = new Date(this.bookRoom.value.checkIn)
-    let checkOut = new Date(this.bookRoom.value.checkOut)
-    let Time = checkOut.getTime() - checkIn.getTime()
-    this.dateDiff = Time/(1000 * 3600 * 24)
-    this.bookRoom.value.numOfNights = Time/(1000 * 3600 * 24)
-    console.log(this.bookRoom.value.numOfNights)
+  checkDateDiff(): any{
+    if (this.bookRoom.value.checkIn && this.bookRoom.value.checkOut !== ''){
+      const checkIn = new Date(this.bookRoom.value.checkIn);
+      const checkOut = new Date(this.bookRoom.value.checkOut);
+      const Time = checkOut.getTime() - checkIn.getTime();
+      // this.dateDiff = Time / (1000 * 3600 * 24);
+      if (Math.sign(Time / (1000 * 3600 * 24)) === -1 || Math.sign(Time / (1000 * 3600 * 24)) ===  0){
+        this.bookRoom.value.numOfNights = '';
+        this.error = 'Check out date invalid';
+      }else {
+        this.bookRoom.value.numOfNights = Time / (1000 * 3600 * 24);
+
+      }
     }
   }
 
